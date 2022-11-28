31bf95b6a552498a9a58bda7eab5c412e456c296
IvorStur <90836443+IvorStur@users.noreply.github.com>
2022-11-28 08:17:22 +0100

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, OneComponent, TwoComponent, ThreeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
